d5c0906e23602ad8bd777c335ce1479570511d7c
elashry <mohamedelashry323@gmail.com>
2018-04-11 01:39:01 +0200

    init

--- app/src/main/java/com/semicolon/Halan/Fragments/CancelledOrdersFragment.java
+++ app/src/main/java/com/semicolon/Halan/Fragments/CancelledOrdersFragment.java
@@ -1,34 +1,114 @@
 package com.semicolon.Halan.Fragments;
 
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
+import com.semicolon.Halan.Adapters.MyOrdersAdapter;
+import com.semicolon.Halan.Models.MyOrderModel;
 import com.semicolon.Halan.R;
+import com.semicolon.Halan.Services.Api;
+import com.semicolon.Halan.Services.Services;
+import com.semicolon.Halan.Services.Tags;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class CancelledOrdersFragment extends Fragment {
 
-    public CancelledOrdersFragment() {
-        // Required empty public constructor
-    }
 
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
+    ArrayList<MyOrderModel> model;
+    MyOrdersAdapter adapter;
+    RecyclerView recyclerView;
+    private LinearLayoutManager mLayoutManager;
+    private ProgressBar progBar;
+    private LinearLayout nodata_container;
+    private SwipeRefreshLayout sr;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cancelled_orders, container, false);
+
+        View view= inflater.inflate(R.layout.fragment_cancelled_orders, container, false);
+
+        recyclerView = view.findViewById(R.id.rec_canceled);
+
+        model = new ArrayList<>();
+
+        mLayoutManager=new LinearLayoutManager(getContext());
+        mLayoutManager.setReverseLayout(true);
+        mLayoutManager.setStackFromEnd(true);
+
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        adapter = new MyOrdersAdapter(getContext(), model);
+        recyclerView.setAdapter(adapter);
+        sr =view. findViewById(R.id.sr);
+        sr.setRefreshing(false);
+
+        progBar = view.findViewById(R.id.progBar);
+        progBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(getContext(),R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+        nodata_container = view.findViewById(R.id.nodata_container);
+        sr.setColorSchemeResources(R.color.colorPrimary);
+        sr.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getDataFromServer();
+            }
+        });
+        getDataFromServer();
+
+
+        return view;
+    }
+
+
+    private void getDataFromServer() {
+        progBar.setVisibility(View.VISIBLE);
+        Services services= Api.getClient(Tags.BASE_URL).create(Services.class);
+        Call<List<MyOrderModel>> call=services.getCanceledOrders();
+        call.enqueue(new Callback<List<MyOrderModel>>() {
+            @Override
+            public void onResponse(Call<List<MyOrderModel>> call, Response<List<MyOrderModel>> response) {
+
+                model.clear();
+                model.addAll( response.body());
+                if (model.size()>0){
+                    adapter.notifyDataSetChanged();
+                    progBar.setVisibility(View.GONE);
+                    sr.setRefreshing(false);
+                    // Toast.makeText(Activities.this, "no activities", Toast.LENGTH_SHORT).show();
+                }else {
+                    progBar.setVisibility(View.GONE);
+                    nodata_container.setVisibility(View.VISIBLE);
+                    sr.setRefreshing(false);
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<MyOrderModel>> call, Throwable t) {
+                nodata_container.setVisibility(View.GONE);
+                sr.setRefreshing(false);
+
+            }
+        });
     }
 
 
--- app/src/main/java/com/semicolon/Halan/Fragments/CurrentOrdersFragment.java
+++ app/src/main/java/com/semicolon/Halan/Fragments/CurrentOrdersFragment.java
@@ -1,12 +1,17 @@
 package com.semicolon.Halan.Fragments;
 
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 
 import com.semicolon.Halan.Adapters.MyOrdersAdapter;
 import com.semicolon.Halan.Models.MyOrderModel;
@@ -29,32 +34,17 @@ public class CurrentOrdersFragment extends Fragment {
     MyOrdersAdapter adapter;
     RecyclerView recyclerView;
     private LinearLayoutManager mLayoutManager;
-
-
-
-
-
-
-    public CurrentOrdersFragment() {
-        // Required empty public constructor
-    }
-
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
+    private ProgressBar progBar;
+    private LinearLayout nodata_container;
+    private SwipeRefreshLayout sr;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
         View view= inflater.inflate(R.layout.fragment_current_orders, container, false);
 
         recyclerView = view.findViewById(R.id.rec_current);
+        sr =view. findViewById(R.id.sr);
 
         model = new ArrayList<>();
 
@@ -67,7 +57,27 @@ public class CurrentOrdersFragment extends Fragment {
 
         adapter = new MyOrdersAdapter(getContext(), model);
         recyclerView.setAdapter(adapter);
+        sr.setRefreshing(false);
 
+        progBar = view.findViewById(R.id.progBar);
+        progBar.getIndeterminateDrawable().setColorFilter(ContextCompat.getColor(getContext(),R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+        nodata_container = view.findViewById(R.id.nodata_container);
+        sr.setColorSchemeResources(R.color.colorPrimary);
+        sr.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getDataFromServer();
+            }
+        });
+        getDataFromServer();
+
+
+        return view;
+    }
+
+
+    private void getDataFromServer() {
+        progBar.setVisibility(View.VISIBLE);
         Services services= Api.getClient(Tags.BASE_URL).create(Services.class);
         Call<List<MyOrderModel>> call=services.getCurrentOrders();
         call.enqueue(new Callback<List<MyOrderModel>>() {
@@ -76,21 +86,27 @@ public class CurrentOrdersFragment extends Fragment {
 
                 model.clear();
                 model.addAll( response.body());
-                adapter.notifyDataSetChanged();
-
+                if (model.size()>0){
+                    adapter.notifyDataSetChanged();
+                    progBar.setVisibility(View.GONE);
+                    sr.setRefreshing(false);
+                    // Toast.makeText(Activities.this, "no activities", Toast.LENGTH_SHORT).show();
+                }else {
+                    progBar.setVisibility(View.GONE);
+                    nodata_container.setVisibility(View.VISIBLE);
+                    sr.setRefreshing(false);
+                }
 
             }
 
             @Override
             public void onFailure(Call<List<MyOrderModel>> call, Throwable t) {
+                nodata_container.setVisibility(View.GONE);
+                sr.setRefreshing(false);
 
             }
         });
-
-        return view;
     }
-
-
 
 
 }
